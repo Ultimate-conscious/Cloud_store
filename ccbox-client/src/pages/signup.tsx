10f2bf88a206a036db1cfb3f3ef166af7e8b7c60
Ultimate-conscious <prlanjewar2002@gmail.com>
2024-08-21 17:08:15 +0530

    Some client side routing done create the logged in user dash board, completed the auth check functionality

--- ccbox-client/src/pages/signup.tsx
+++ ccbox-client/src/pages/signup.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { ButtonStyled } from "../components/buttonstyled"
 import { InputBoxWithLabel } from "../components/inputbox-label"
+import { useNavigate } from "react-router-dom";
 
 export function SignupPage(){
     const [name,setName] = useState("")
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+
     return <div>
         signup
         <InputBoxWithLabel label="Full Name" placeholder="John Doe"  handler={(e)=>{
@@ -17,6 +20,14 @@ export function SignupPage(){
         <InputBoxWithLabel label="Password" placeholder="*******" handler={(e)=>{
             setPassword(e.target.value)
         }}/>
-        <ButtonStyled title="Signup" handler={()=>{ alert('bhai button dab gaya')}} />
+        <ButtonStyled title="Signup" handler={async ()=>{
+            const response :any = await axios.post("http://localhost:3000/user/signup",{
+                name,
+                email,
+                password
+            })
+            localStorage.setItem("token",response.data.token)
+            navigate('/dashboard')
+        }} />
     </div>
 }
